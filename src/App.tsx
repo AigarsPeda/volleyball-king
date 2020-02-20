4361f93da37d74874e00def86c977a70cccf7f42
Aigars Peda <aigarspeda@gmail.com>
2020-02-20 13:19:33 +0200

    refactoring code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { ThemeProvider } from "@material-ui/core/styles";
 import theme from "./ui/Theme";
 import Form from "./components/Form";
 
 const App: React.FC = () => {
-  const [name1, setName1] = useState<string>("");
-  const [name2, setName2] = useState<string>("");
-  const [name3, setName3] = useState<string>("");
-  const [name4, setName4] = useState<string>("");
-  const [name5, setName5] = useState<string>("");
+  const [showingForm, setShowingForm] = useState(true);
+  const [playerArray, setPlayerArray] = useState<Player[]>();
 
-  const [playerArray, setPlayerArray] = useState<string[]>();
-
-  const namesObj = { name1, name2, name3, name4, name5 };
-  const setNamesObj = { setName1, setName2, setName3, setName4, setName5 };
+  const savePlayerArrayToSate = (newArray: Player[]) => {
+    setPlayerArray([...newArray]);
+    setShowingForm(false);
+  };
 
   return (
     <ThemeProvider theme={theme}>
       {console.log(playerArray)}
-      <Form namesObj={namesObj} setNamesObj={setNamesObj} />
+      {showingForm && <Form savePlayerArrayToSate={savePlayerArrayToSate} />}
     </ThemeProvider>
   );
 };
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, TextField, makeStyles, Theme } from "@material-ui/core";
 
 const useStyles = makeStyles((theme: Theme) => ({
@@ -14,64 +14,85 @@ const useStyles = makeStyles((theme: Theme) => ({
 }));
 
 interface IForm {
-  setNamesObj: {
-    setName1: React.Dispatch<React.SetStateAction<string>>;
-    setName2: React.Dispatch<React.SetStateAction<string>>;
-    setName3: React.Dispatch<React.SetStateAction<string>>;
-    setName4: React.Dispatch<React.SetStateAction<string>>;
-    setName5: React.Dispatch<React.SetStateAction<string>>;
-  };
-  namesObj: {
-    name1: string;
-    name2: string;
-    name3: string;
-    name4: string;
-    name5: string;
-  };
+  savePlayerArrayToSate: (newArray: Player[]) => void;
 }
 
 const Form: React.FC<IForm> = props => {
-  const { namesObj, setNamesObj } = props;
-  const { name1, name2, name3, name4, name5 } = namesObj;
-  const { setName1, setName2, setName3, setName4, setName5 } = setNamesObj;
+  const [name1, setName1] = useState<string>("");
+  const [name2, setName2] = useState<string>("");
+  const [name3, setName3] = useState<string>("");
+  const [name4, setName4] = useState<string>("");
+  const [name5, setName5] = useState<string>("");
   const classes = useStyles();
+  const { savePlayerArrayToSate } = props;
+
+  const makeArrayOfPlayers = (e: React.FormEvent) => {
+    e.preventDefault();
+    const player1: Player = {
+      id: 1,
+      name: name1,
+      bigPoints: 0,
+      smallPoints: 0
+    };
+    const player2: Player = {
+      id: 2,
+      name: name2,
+      bigPoints: 0,
+      smallPoints: 0
+    };
+    const player3: Player = {
+      id: 3,
+      name: name3,
+      bigPoints: 0,
+      smallPoints: 0
+    };
+    const player4: Player = {
+      id: 4,
+      name: name4,
+      bigPoints: 0,
+      smallPoints: 0
+    };
+    const player5: Player = {
+      id: 5,
+      name: name5,
+      bigPoints: 0,
+      smallPoints: 0
+    };
+    const newPlayerArray = [player1, player2, player3, player4, player5];
+    savePlayerArrayToSate(newPlayerArray);
+  };
   return (
-    <form className={classes.root}>
+    <form className={classes.root} onSubmit={makeArrayOfPlayers}>
       <TextField
-        id="name"
-        label="Name"
+        label="Spēlētāja vārds"
         required
         value={name1}
         onChange={e => setName1(e.target.value)}
         margin="normal"
       />
       <TextField
-        id="name"
-        label="Name"
+        label="Spēlētāja vārds"
         required
         value={name2}
         onChange={e => setName2(e.target.value)}
         margin="normal"
       />
       <TextField
-        id="name"
-        label="Name"
+        label="Spēlētāja vārds"
         required
         value={name3}
         onChange={e => setName3(e.target.value)}
         margin="normal"
       />
       <TextField
-        id="name"
-        label="Name"
+        label="Spēlētāja vārds"
         required
         value={name4}
         onChange={e => setName4(e.target.value)}
         margin="normal"
       />
       <TextField
-        id="name"
-        label="Name"
+        label="Spēlētāja vārds"
         required
         value={name5}
         onChange={e => setName5(e.target.value)}
