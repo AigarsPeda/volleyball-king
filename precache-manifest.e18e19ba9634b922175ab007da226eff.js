8a348496d5df4d82d6e086de8f51db67199d58a3
Aigars Peda <aigarspeda@gmail.com>
2020-02-25 11:17:23 +0200

    Updates

---
+++ precache-manifest.e18e19ba9634b922175ab007da226eff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13686658027dbab4390c825b64c1f2c0",
+    "url": "/volleyball-king/index.html"
+  },
+  {
+    "revision": "33ad8274e7a03b732461",
+    "url": "/volleyball-king/static/css/main.e999132f.chunk.css"
+  },
+  {
+    "revision": "9a887e46ab3d1b3c6db4",
+    "url": "/volleyball-king/static/js/2.340c2181.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/volleyball-king/static/js/2.340c2181.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33ad8274e7a03b732461",
+    "url": "/volleyball-king/static/js/main.6fcbddc5.chunk.js"
+  },
+  {
+    "revision": "183da78ea0366fd55822",
+    "url": "/volleyball-king/static/js/runtime-main.6e73ac28.js"
+  }
+]);
