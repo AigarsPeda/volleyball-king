bef41e560c8a6a678d04a2130acf88be267bcee1
Aigars Peda <aigarspeda@gmail.com>
2020-02-25 06:21:29 +0200

    adedd localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { ThemeProvider } from "@material-ui/core/styles";
 import theme from "./ui/Theme";
 // import { Button } from "@material-ui/core";
@@ -8,7 +8,13 @@ import Game from "./components/Game";
 const NUMBER_OF_PLAYERS = 5;
 
 const App: React.FC = () => {
-  const [playerArray, setPlayerArray] = useState<Player[]>([]);
+  const [playerArray, setPlayerArray] = useState<Player[]>(() =>
+    JSON.parse(localStorage.getItem("playerArray") || "[]")
+  );
+
+  useEffect(() => {
+    localStorage.setItem("playerArray", JSON.stringify(playerArray));
+  }, [playerArray]);
 
   const savePlayerArrayToSate = (name: string) => {
     const newPlayer: Player = {
@@ -34,9 +40,8 @@ const App: React.FC = () => {
         {playerArray?.length < NUMBER_OF_PLAYERS ? (
           <Form savePlayerArrayToSate={savePlayerArrayToSate} />
         ) : (
-          <h2>Tabula</h2>
+          <Game playerArray={playerArray} setPlayerArray={setPlayerArray} />
         )}
-        <Game playerArray={playerArray} setPlayerArray={setPlayerArray} />
       </div>
     </ThemeProvider>
   );
