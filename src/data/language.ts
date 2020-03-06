6529e50bd0ef602dca7ad4805635fcb45167b80e
Aigars Peda <aigarspeda@gmail.com>
2020-03-06 08:51:31 +0200

    started adding English

---
+++ src/data/language.ts
@@ -0,0 +1,41 @@
+export const mainText: MainText = {
+  eng: {
+    text:
+      "Hi! My name is Aigars, I am front end developer and I’m looking for a job",
+    buttonNewTournament: "New Tournament",
+    buttonBack: "Back",
+    points: "Points",
+    wins: "Wins",
+    gameNumber: "Game Nr",
+    nextGame: "Next game",
+    playersName: "Players name",
+    editPlayersName: "Edit players name",
+    addPlayer: "Add player",
+    four: "Four",
+    five: "Five",
+    count: "Count",
+    howManyPlayers: "How many players",
+    tournamentEnd: "Tournament has finished",
+    newTournaments: "START NEW TOURNAMENT",
+    newTournamentsConfirm: "Do You really want to start a new tournament?"
+  },
+  lv: {
+    text: `Čau! Mani sauc Aigars, un es meklēju front end izstrādātāja darbu.`,
+    buttonNewTournament: "JAUNS TURNĪRS",
+    buttonBack: "Atpakaļ",
+    points: "Punkti",
+    wins: "Uzvaras",
+    gameNumber: "Spēle Nr",
+    nextGame: "Nākamā spēle",
+    playersName: "Spēlētāja vārds",
+    editPlayersName: "Labot spēlētāja vārdu",
+    addPlayer: "Pievienot spēlētāju",
+    four: "Četri",
+    five: "Pieci",
+    count: "Skaits",
+    howManyPlayers: "Cik spēlētāju būs?",
+    tournamentEnd: "Turnīrs Beidzies!",
+    newTournaments: "SĀKT JAUNU TŪRNĪRU",
+    newTournamentsConfirm: "Vai Tu tiešām vēlies sākt jaunu turnīru?"
+  }
+};
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -6,3 +6,28 @@ type Player = {
   bigPoints: number;
   smallPoints: number;
 };
+
+type MainText = {
+  eng: Language;
+  lv: Language;
+};
+
+type Language = {
+  text: string;
+  buttonNewTournament: string;
+  buttonBack: string;
+  points: string;
+  wins: string;
+  gameNumber: string;
+  nextGame: string;
+  playersName: string;
+  editPlayersName: string;
+  addPlayer: string;
+  four: string;
+  five: string;
+  count: string;
+  howManyPlayers: string;
+  tournamentEnd: string;
+  newTournaments: string;
+  newTournamentsConfirm: string;
+};
